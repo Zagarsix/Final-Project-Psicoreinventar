a424a6749e15037d4a0477aa361dfcc73166d611
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-08 11:45:27 -0400

    Update card component

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,17 +1,15 @@
 const Card = ({ img, imgLabel, title, description }) => {
   return (
-    <div className="text-center">
-      <div className="card h-100" style={{ width: "18rem" }}>
-        <img
-          src={img}
-          className="card-img-top mx-auto"
-          alt={imgLabel}
-          style={{ height: "150px" }}
-        />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-        </div>
+    <div className="card h-100 text-center" style={{ width: "18rem" }}>
+      <img
+        src={img}
+        className="card-img-top mx-auto"
+        alt={imgLabel}
+        style={{ height: "150px" }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
       </div>
     </div>
   );
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Context } from "../store/appContext";
 import Header from "../components/Header";
 import Card from "../components/Card";
+import SpecialistCard from "../components/SpecialistCard";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
@@ -20,27 +21,40 @@ const Home = () => {
       />
       <h2 className="fw-bold text-center">Cómo funciona</h2>
       <section className="card-section bg-warning">
-        <div className="container d-flex justify-content-center align-items-center">
-          <Card
-            img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558a4ebfab0b487ff53146_steps-icon1.svg"
-            imgLabel="specialists-icon"
-            title="1. Elige al especialista"
-            description="Puedes elegir dentro de los diferentes perfiles con los que contamos."
-          />
-          <Card
-            img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558af73f29166024b10686_steps-icon2.svg"
-            imgLabel="specialists-icon"
-            title="2. Elige un horario"
-            description="Con base a los horarios disponibles, elige el que más te acomode."
-          />
-          <Card
-            img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558af721df57b8f5bc098b_steps-icon3.svg"
-            imgLabel="specialists-icon"
-            title="3. Ingresa tus datos"
-            description="Vamos a necesitar tu nombre, email, teléfono y medio de pago"
-          />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 offset-md-1 py-3">
+              <div className="row">
+                <div className="col-md-4">
+                  <Card
+                    img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558a4ebfab0b487ff53146_steps-icon1.svg"
+                    imgLabel="specialists-icon"
+                    title="1. Elige al especialista"
+                    description="Puedes elegir dentro de los diferentes perfiles con los que contamos."
+                  />
+                </div>
+                <div className="col-md-4">
+                  <Card
+                    img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558af73f29166024b10686_steps-icon2.svg"
+                    imgLabel="specialists-icon"
+                    title="2. Elige un horario"
+                    description="Con base a los horarios disponibles, elige el que más te acomode."
+                  />
+                </div>
+                <div className="col-md-4">
+                  <Card
+                    img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/62558af721df57b8f5bc098b_steps-icon3.svg"
+                    imgLabel="specialists-icon"
+                    title="3. Ingresa tus datos"
+                    description="Vamos a necesitar tu nombre, email, teléfono y medio de pago"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+
       <div className="text-center mt-5">
         <h1>Hello, home here!!!</h1>
         <div className="alert alert-info">
