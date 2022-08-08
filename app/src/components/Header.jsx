844b625c82dc807ffd987c3cfbb28a93a8fc0a56
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-08 10:21:58 -0400

    Add header component

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = ({
+  title,
+  title_highlight,
+  description,
+  btnLabel,
+  img,
+  imgLabel,
+}) => {
+  return (
+    <main className="main-section w-100 h-100 p-5">
+      <div className="container">
+        <div className="d-sm-flex justify-content-center align-items-center">
+          <div className="left-column w-50">
+            <h1 className="fw-bold">
+              {title} <span className="text-primary">{title_highlight}</span>
+            </h1>
+            <p className="my-4">{description}</p>
+            <button className="btn btn-primary">{btnLabel}</button>
+          </div>
+          <div className="main-image">
+            <img src={img} className="img-fluid w-100" alt={imgLabel} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Header;
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import { useContext } from "react";
 import { Context } from "../store/appContext";
+import Header from "../components/Header";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
   return (
     <>
+      <Header
+        title="Project"
+        title_highlight="Incoming"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Molestias atque itaque illo enim quidem at ad voluptatibus,
+        nesciunt quia alias optio debitis quam suscipit, quod animi
+        delectus veritatis! Saepe, maxime."
+        btnLabel="Agenda una cita"
+        img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/6255ce36e7efb7be217ba6c3_woman-laptop-couch.png"
+        imgLabel="Doctor"
+      />
       <div className="text-center mt-5">
         <h1>Hello, home here!!!</h1>
         <div className="alert alert-info">
