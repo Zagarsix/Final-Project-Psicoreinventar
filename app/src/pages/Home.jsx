e1abe191fd6c609c6a492a4217d6b15e4b10d308
PauFugit <paulinefugit@gmail.com>
2022-08-10 16:10:13 +0000

    cardclient reviews

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Card from "../components/Card";
 import SpecialistCard from "../components/SpecialistCard";
 import LeftHeader from "../components/LeftHeader";
+import CardClient from "../components/CardClient";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
@@ -63,6 +64,64 @@ const Home = () => {
         title_highlight="más felices"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id illum dignissimos eos non quibusdam cupiditate, omnis nesciunt officia molestiae corrupti odio sint sit doloremque incidunt soluta, et nemo harum. Facere!"
       />
+{/*Clients review */}
+<section>
+        <div className="container w-100 h-100">
+          <div className="d-sm-flex align-items-center">
+            <div className="col-md-5">
+              <h1 className="fw-bold">
+                Lo que nuestros clientes opinan
+              </h1>
+              <p className="my-4">Porta quam magna rutrum phasellus vel quisque at mauris fermentum ante, morbi senectus porttitor pulvinar etiam eleifend felis leo posuere.</p>
+              <button className="btn btn-primary">Agendar sesión</button>
+            </div>
+
+            <div className="container col-md-6">
+              <div className="row">
+                <CardClient
+                  img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/620d509f475bbb766fc9b204_4-p-500.png"
+                  imgLabel="clientpicture"
+                  clientsName="Laura"
+                  clientsAge="27 años"
+                  review="Congue malesuada iaculis dictumst convallis pharetra augue elementum euismod felis vel viverra pellentesque rhoncus"
+                />
+              </div>
+              
+              <div className="row" >
+                <CardClient
+                  img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/620d508cd3237eb424b497fc_2-p-500.png"
+                  imgLabel="clientpicture"
+                  clientsName="Mateo"
+                  clientsAge="30 años"
+                  review="Congue malesuada iaculis dictumst convallis pharetra augue elementum euismod felis vel viverra pellentesque rhoncus"
+                />
+              </div>
+
+              <div className="row">
+                <CardClient
+                  img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/620d50e0f373ab219ce4dc84_5-p-500.png"
+                  imgLabel="clientpicture"
+                  clientsName="Vanessa"
+                  clientsAge="23 años"
+                  review="Congue malesuada iaculis dictumst convallis pharetra augue elementum euismod felis vel viverra pellentesque rhoncus"
+                />
+              </div>
+
+              <div className="row">
+                <CardClient
+                  img="https://uploads-ssl.webflow.com/6165ed3d14364c7f8297fe2e/620d5115844b6721a99f62e6_6-p-500.png"
+                  imgLabel="clientpicture"
+                  clientsName="Luis"
+                  clientsAge="35 años"
+                  review="Congue malesuada iaculis dictumst convallis pharetra augue elementum euismod felis vel viverra pellentesque rhoncus"
+                />
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
 
       <div className="text-center mt-5">
         <h1>Hello, home here!!!</h1>
