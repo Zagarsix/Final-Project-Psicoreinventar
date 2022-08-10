405793b943af95160b6989f0e0f9a7849fa6e509
MrMandujano <franciscomandujanoe@gmail.com>
2022-08-10 09:46:56 -0400

    change on specialist card

--- app/src/components/SpecialistCard.jsx
+++ app/src/components/SpecialistCard.jsx
@@ -1,21 +1,44 @@
 const SpecialistCard = () => {
   return (
-    <div className="card border border-danger" id="specialistCard" style={{ width: "18rem" }}>
-      <img src="http://placekitten.com/100/100" id="specialistCardImg" className="card-img-top rounded-circle" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Specialist Name</h5>
-        <p className="card-text" id="specialistCardList">
-          <ul>
-            <li>Adolescentes y adultos, cuestiones de pareja y familiares</li>
-            <li>18+ años de terapia clínica</li>
-            <li>Maestría en Psicoterapia Existencial Humanista y Licenciatura en Psicología</li>
-          </ul>
-        </p>
-        <a href="#" className="btn btn-primary">
-          Agendar sesión
-        </a>
-      </div>
-    </div>
+  
+   <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div class="card ">
+                            <img src="http://placekitten.com/100/100" class="card-img-top rounded-circle photo mx-auto m-3" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-md-4">
+                        <div class="card ">
+                            <img src="http://placekitten.com/100/100" class="card-img-top rounded-circle photo mx-auto m-3" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-md-4">
+                        <div class="card ">
+                            <img src="http://placekitten.com/100/100" class="card-img-top rounded-circle photo mx-auto m-3" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+   </div>
+   
   )
 }
 
