553099a5bd3f62e09a96d24139d81898a2fcf531
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-17 10:24:00 -0400

    Update SpecialistCard

--- app/src/components/SpecialistCard.jsx
+++ app/src/components/SpecialistCard.jsx
@@ -1,22 +1,39 @@
-const SpecialistCard = () => {
+import "../styles/Card.css";
+
+const SpecialistCard = ({
+  img,
+  imgLabel,
+  title,
+  experience,
+  education,
+  specialization1,
+  specialization2,
+}) => {
   return (
-    <div className="card border border-danger" id="specialistCard" style={{ width: "18rem" }}>
-      <img src="http://placekitten.com/100/100" id="specialistCardImg" className="card-img-top rounded-circle" alt="..." />
+    <div className="card h-100" style={{ width: "18rem" }}>
+      <img
+        src={img}
+        className="card-img-top rounded-circle photo mx-auto m-3"
+        alt={imgLabel}
+        style={{ height: "135px" }}
+      />
       <div className="card-body">
-        <h5 className="card-title">Specialist Name</h5>
-        <p className="card-text" id="specialistCardList">
-          <ul>
-            <li>Adolescentes y adultos, cuestiones de pareja y familiares</li>
-            <li>18+ años de terapia clínica</li>
-            <li>Maestría en Psicoterapia Existencial Humanista y Licenciatura en Psicología</li>
-          </ul>
-        </p>
-        <a href="#" className="btn btn-primary">
-          Agendar sesión
-        </a>
+        <h5 className="card-title">{title}</h5>
+        <ul className="doctor-info">
+          <li className="li-item">{experience}</li>
+          <li className="li-item">{education}</li>
+        </ul>
+        <div className="specialization-info">
+          <span className="badge rounded-pill text-bg-primary">
+            {specialization1}
+          </span>
+          <span className="badge rounded-pill text-bg-primary">
+            {specialization2}
+          </span>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpecialistCard
+export default SpecialistCard;
