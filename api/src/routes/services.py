1fea5163b0bee85e64b1b0075e1e1a7a03cb729c
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-18 11:59:40 -0400

    Add create service route

---
+++ api/src/routes/services.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request, url_for, Blueprint
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from models import User, Service
+
+services = Blueprint('services', __name__)
+
+@services.route('/service', methods=['POST'])
+def create_service():
+    name = request.json.get('name')
+    description = request.json.get('description')
+    price = request.json.get('price')
+    service_image = request.json.get('service_image')
+
+     # check if all is filled
+    if not name: return jsonify({'status': 'failed', 'message': 'Name is required', 'data': None}), 400
+    if not description: return jsonify({'status': 'failed', 'message': 'Description is required', 'data': None}), 400
+    if not price: return jsonify({'status': 'failed', 'message': 'Price is required', 'data': None}), 400
+    if not service_image: return jsonify({'status': 'failed', 'message': 'Service Image is required', 'data': None}), 400
+
+    service = Service()
+
+    service.name = name
+    service.description = description
+    service.price = price
+    service.service_image = service_image
+
+    service.save()
+
+    # if create service succeded
+    if service: return jsonify({'status': 'success', 'message': 'Service created successfully', 'data': None}), 200
+    else: return jsonify({'status': 'failed', 'message': 'Error creating service, please try again', 'data': None}), 200
+   
+
+
+@services.route('/edit_service', methods=['PUT'])
+def edit_service():
+    pass
+
+@services.route('/delete_service', methods=['PUT'])
+def edit_service():
+    pass
