43f7482b2be281cf6b94b1e403a2e6a49210d753
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-18 01:11:27 -0400

    Update models adding relationships to appointment

--- api/src/models.py
+++ api/src/models.py
@@ -42,6 +42,8 @@ class User(db.Model):
     phone = db.Column(db.String(100), nullable=False)
     is_active = db.Column(db.Boolean(), default=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    #Relationships of client
+    appointments = db.relationship('Appointment', backref="user")
     # Doctor model (making nullable true for client)
     experience = db.Column(db.String(100))
     education = db.Column(db.String(100))
@@ -49,6 +51,7 @@ class User(db.Model):
     specialization2 = db.Column(db.String(50))
     # image url, not uploading
     image = db.Column(db.String(250))
+ 
 
 
     def serialize(self):
@@ -84,20 +87,29 @@ class Appointment(db.Model):
     __tablename__ = 'appointments'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
-    # pacient has role 3, doctor has role 2
-    # current user (pacient) id
-    pacient_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
-    # doctor being selected on dropdown id
-    doctor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    #!!!!!!!!!!!! GETTING ERROR BECAUSE there are multiple foreign key paths linking the tables.  
+    # pacient_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
+    # # doctor being selected on dropdown id
+    # doctor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    # relationship with the user booking the appointmnet, doctors (users with role 2)
-    # and with the service being chosen
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # relationing the appointment with the service being chosen
+    service = db.relationship('Service', backref="appointment", uselist=False)
+    # creating an invoice for the appointment
+    invoice = db.relationship('Invoice', backref="appointment", uselist=False)
 
+ 
     def serialize(self):
         return {
             'id': self.id,
+            'date': self.date,
+            'user_id': self.user_id,
             # 'pacient_id': self.pacient_id,
-            # 'doctors_id': self.doctors_id
+            # 'doctor_id': self.doctor_id,
+            # Getting the name of the service chosen for the appointment
+            'service': self.service.name,
+            # Getting all the data of the invoice
+            'invoice': self.invoice.serialize()
         }
 
     def save(self):
@@ -119,6 +131,8 @@ class Service(db.Model):
     price = db.Column(db.Integer, nullable=False) 
     service_image = db.Column(db.String(256), nullable=False)
     stripe_id = db.Column(db.String(100), nullable=False, unique=True)
+    # Appointment relationship
+    appointment_id = db.Column(db.Integer, db.ForeignKey('appointments.id'))
 
     def serialize(self):
         return {
@@ -127,7 +141,19 @@ class Service(db.Model):
             'description': self.description,
             'price': self.price,
             'service_image': self. service_image,
-            'stripe_id': self.stripe_id
+            'stripe_id': self.stripe_id,
+        }
+
+    # Serialize with appointment
+    def serialize_with_appointment(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'service_image': self. service_image,
+            'stripe_id': self.stripe_id,
+            'appointment': self.appointment.id
         }
 
     def save(self):
@@ -146,15 +172,17 @@ class Invoice(db.Model):
     __tablename__ = 'invoices'
     id = db.Column(db.Integer, primary_key=True)
     date_of_purchase = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    pacient_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     stripe_id = db.Column(db.String(100), nullable=False)
     # relationship with the appointment
+    appointment_id = db.Column(db.Integer, db.ForeignKey('appointments.id'))
 
     def serialize(self):
         return {
             'id': self.id,
             'date_of_purchase': self.date_of_purchase,
-            'user_id': self.user_id
+            'pacient_id': self.pacient_id,
+            'appointment_id': self.appointment_id
         }
 
     def save(self):
@@ -168,30 +196,30 @@ class Invoice(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-# class Payment(db.Model):
-#     __tablename__ = 'payments'
-#     # id = db.Column(db.Integer, db.ForeignKey('Invoices.id'), primary_key=True)
-#     amount = db.Column(db.Integer, nullable=False, unique=True)
-#     date_of_purchase = db.Column(db.String(100), nullable=False)
-#     payment_method = db.Column(db.String(100), nullable=False, unique=True)
-#     stripe_id = db.Column(db.String(100), nullable=False)
+class Payment(db.Model):
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    date_of_purchase = db.Column(db.String(100), nullable=False)
+    payment_method = db.Column(db.String(100), nullable=False, unique=True)
+    stripe_id = db.Column(db.String(100), nullable=False)
 
-#     def serialize(self):
-#         return {
-#             'id': self.id,
-#             'amount': self.amount,
-#             'date_of_purchase': self.date_of_purchase,
-#             'payment_method': self.payment_method,
-#             'stripe_id': self.stripe_id
-#         }
+    def serialize(self):
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'date_of_purchase': self.date_of_purchase,
+            'payment_method': self.payment_method,
+            'stripe_id': self.stripe_id
+        }
 
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
     
-#     def update(self):
-#         db.session.commit()
+    def update(self):
+        db.session.commit()
 
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
