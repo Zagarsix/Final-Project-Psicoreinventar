b9c35a02ea3a95cde3adc9c4c1b5a79980ebe880
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-24 12:25:44 -0400

    Add delete client functionality on admin dashboard

--- app/src/components/TableDataClient.jsx
+++ app/src/components/TableDataClient.jsx
@@ -1,7 +1,54 @@
-import ModalDelete from "./Modal/ModalDelete";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import ModalEdit from "./Modal/ModalEdit";
 
 const TableDataClient = ({ name, lastname, email, index }) => {
+  const { store, actions } = useContext(Context);
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const [clientId, setClientId] = useState(null);
+
+  useEffect(() => {
+    console.log(clientId);
+  }, [clientId]);
+
+  const handleDeleteClient = async (e) => {
+    // Fetching data from API
+    const response = await fetch(
+      `${store.apiURL}/api/delete_user/${clientId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${store.currentUser?.access_token}`,
+        },
+      }
+    );
+
+    const { status, message, data } = await response.json();
+
+    console.log(data);
+
+    if (status === "failed") {
+      toast.error(message);
+    }
+
+    if (status === "success") {
+      actions.getClients();
+      Swal.fire({
+        icon: "success",
+        title: message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
   return (
     <tbody className="table-group-divider" style={{ fontSize: "13px" }}>
       <tr>
@@ -19,11 +66,38 @@ const TableDataClient = ({ name, lastname, email, index }) => {
           <div className="botones">
             <div className="d-flex align-items-center">
               <ModalEdit editWord="Editar" editWhat="cliente" />
-              <ModalDelete
-                deleteWord="Eliminar"
-                connector="al"
-                deleteWhat="cliente"
-              />
+              <div className="delete-user-modal">
+                <Button
+                  color="light"
+                  onClick={() => {
+                    toggle();
+                    setClientId(index);
+                  }}
+                  index={index}
+                >
+                  <i className="fa-solid fa-trash-can"></i>
+                </Button>
+                <Modal isOpen={modal} fade={false} toggle={toggle}>
+                  <ModalHeader toggle={toggle}>Eliminar paciente</ModalHeader>
+                  <ModalBody>
+                    Estas seguro de qué quieres Eliminar al paciente?
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      color="danger"
+                      onClick={(e) => {
+                        toggle();
+                        handleDeleteClient(e);
+                      }}
+                    >
+                      Confirmar
+                    </Button>
+                    <Button color="secondary" onClick={toggle}>
+                      Cancelar
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
             </div>
           </div>
         </td>
