538a372bb0e1ada86e23feddf58fb9e7f127b763
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-31 21:00:07 -0400

    Update layout, removing things not used

--- app/src/Layout.js
+++ app/src/Layout.js
@@ -4,7 +4,6 @@ import injectContext from "./store/appContext";
 import Home from "./pages/Home";
 import Specialists from "./pages/Specialists";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar";
 import NavbarNew from "./components/NewNavbar";
 import SuperAdmin from "./pages/Dashboard/SuperAdmin";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -24,9 +23,9 @@ import Footer from "./components/Footer";
 const Layout = () => {
   const FooterCheck = () => {
     const { pathname } = useLocation();
-    console.log(pathname);
-    // only display footer in these routes
+    // console.log(pathname);
 
+    // only display footer in these routes
     if (pathname === "/" || pathname === "*" || pathname === "/specialists") {
       return <Footer />;
     } else {
@@ -36,21 +35,22 @@ const Layout = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      {/* <Navbar /> */}
       <NavbarNew />
       <Routes>
+        {/* PAGES */}
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/specialists" element={<Specialists />} />
+        {/* DASHBOARD  */}
         <Route path="/admin" element={<SuperAdmin />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/doctores" element={<Doctores />} />
         <Route path="/pacientes" element={<Pacientes />} />
-        {/* WIZARD COMPONENTS */}
+        {/* AUTH COMPONENTS */}
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/services" element={<Services />} /> */}
+        {/* WIZARD COMPONENTS */}
         <Route path="/appointment" element={<Appointment />} />
         {/* EDIT ROUTES */}
         <Route path="/edit/service/:id" element={<FormEditService />} />
@@ -59,9 +59,7 @@ const Layout = () => {
         <Route path="/edit/appointment/:id" element={<FormEditAppointment />} />
       </Routes>
       <ToastContainer />
-      {/* <Footer /> */}
       <FooterCheck />
-      {/* {location.pathname !== "/dashboard" || ("/appointment" && <Footer />)} */}
     </BrowserRouter>
   );
 };
